986c6684611b354abe15fcb00544b3e441590200
enzoesper <enzoesper@hotmail.com>
2019-03-21 23:25:24 -0300

    se resetean las variables y se hace funcionar el dado

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 soundScore = 0;
 activePlayer = 1;
 
-// Numer aleatorio del 1 al 6 (para el dado)
-dice = ((Math.floor(Math.random() * 6)) + 1) ;
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#current-' + activePlayer).textContent = dice; 
+document.getElementById('score-0').textContent = 0 ;
+document.getElementById('score-1').textContent = 0 ;
+document.getElementById('current-0').textContent = 0 ;
+document.getElementById('current-1').textContent = 0 ;
 
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  var dice;
+  // Numero aleatorio del 1 al 6 (para el dado)
+  dice = ((Math.floor(Math.random() * 6)) + 1) ;
+
+  diceDOM = document.querySelector('.dice') ;
+  // Aparece el dado una vez que se hace click en el boton
+  diceDOM.style.display = 'block';
+  // coloca cada imagen dependiendo de lo que contenga la variable aleatoria
+  diceDOM.src = "dice-" + dice + ".png";
+
+
+})
+
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice; 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '<em>';
-
-var x = document.querySelector('#score-1').textContent ;
-console.log(x);
