6a204956179f5c19cbfd4b975a43c493761cd3bd
enzoesper <enzoesper@hotmail.com>
2019-03-22 14:31:12 -0300

    Implementacion funcionalidad boton New Game

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = 0 ;
-document.getElementById('score-1').textContent = 0 ;
-document.getElementById('current-0').textContent = 0 ;
-document.getElementById('current-1').textContent = 0 ;
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
   var dice;
@@ -79,6 +70,32 @@ function nextPlayer(){
   }, 500);
 };
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = 0 ;
+  document.getElementById('score-1').textContent = 0 ;
+  document.getElementById('current-0').textContent = 0 ;
+  document.getElementById('current-1').textContent = 0 ;
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  
+  //Se remueve la clase 'active' para que no se vaya acumulando la clase cada vez que se reinicia el juego
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
+}
 
 
 
